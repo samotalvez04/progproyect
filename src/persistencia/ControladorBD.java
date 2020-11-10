59fbd28408c94adca395a6b7bd3c34edc69ba635
samotalvez04 <56727022+samotalvez04@users.noreply.github.com>
2020-11-10 21:18:36 -0200

    New Commit

--- src/persistencia/ControladorBD.java
+++ src/persistencia/ControladorBD.java
@@ -1,5 +1,30 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ControladorBD {
+	
+	public Connection getMySqlConnection() throws Exception {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/proyect?serverTimezone=UTC", "root", "");
+        } catch (Exception r) {
+
+        }
+        return con;
+    }
+	
+	public ResultSet listarMaterias() throws Exception {
+		ResultSet res;
+		Connection con = getMySqlConnection();
+        Statement st;
+        st = con.createStatement();
+        res = st.executeQuery("SELECT * FROM materia");
+        return res;
+	}
 
 }
--- src/proyectprog/ControladorLogic.java
+++ src/proyectprog/ControladorLogic.java
@@ -1,5 +1,15 @@
 package proyectprog;
 
+import java.sql.ResultSet;
+
+import persistencia.ControladorBD;
+
 public class ControladorLogic {
+	ControladorBD controladorbd = new ControladorBD();
+		public ResultSet listarMaterias() throws Exception {
+			return controladorbd.listarMaterias();
+		}
+		
+
 
 }
